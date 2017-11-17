4de950dd511cafa5c2117c2a7acf8ed1c6b83ad3
Kyle Gullicksen <gulli003@cougars.csusm.edu>
2017-11-17 15:31:56 -0800

    Some code cleaning

--- nfaeRemoveEC.cpp
+++ nfaeRemoveEC.cpp
@@ -10,7 +10,9 @@ using namespace std;
 // transform NFAe into NFA  (nfa-e.txt into nfa.txt)
 
 TransitionTable nfa;
+
 unordered_set<char> alphas;
+
 int maxState;
 
 // ----- put utility functions here ----------------------------------------
@@ -22,21 +24,16 @@ void buildnfa()
     ifstream input("nfa-e.txt");
     vector<string> whitespaceSplit;
     Transition transition;
-    char transitionChar = 'e';
-
     vector<Transition> transitions;
-
     for(string currentLine; getline(input, currentLine);)
     {
         whitespaceSplit.clear();
         Utilities::split(currentLine, ' ', whitespaceSplit);
-
         if(whitespaceSplit.size() < 4)
         {
             cout << "Line [" << currentLine << "] was malformed. Skipping." << endl;
             continue;
         }
-
         transition.sourceState = stoi(whitespaceSplit.at(0));
         transition.transitionChars.push_back(whitespaceSplit.at(1)[0]);
 
@@ -44,10 +41,8 @@ void buildnfa()
         string terminatingChar = "-1";
         for(int index = 2; index < whitespaceSplit.size() && whitespaceSplit[index] != terminatingChar; ++index)
             transition.targetStates.push_back(stoi(whitespaceSplit.at(index)));
-
         transitions.push_back(transition);
     }
-
     nfa.populate(transitions);
 
     //Get all the alphas while we have the transitons saved
@@ -59,38 +54,32 @@ void buildnfa()
     maxState = nfa.getMaxState();
 }
 
-
-vector<int> & addTransitionsFromMatchingTargetChar(int currentState, char targetChar, vector<int>& results)
+vector<int>& addTransitionsFromMatchingTargetChar(int currentState, char targetChar, vector<int>& results)
 {
-    vector<int> * targetTransitions = nfa.getTransitions(currentState, targetChar);
-
+    vector<int>* targetTransitions = nfa.getTransitions(currentState, targetChar);
     if(targetTransitions != nullptr)
         for(int stateNumber : *targetTransitions)
         {
             results.push_back(stateNumber);
             addTransitionsFromMatchingTargetChar(stateNumber, EPSILON, results);
         }
-
     return results;
 }
 
-void exportToFile(const char * filename, vector<Transition> & transitions)
+void exportToFile(const char* filename, vector<Transition>& transitions)
 {
     //If we've already exported our results before, delete them
     remove(filename);
 
     //Create the file
     ofstream results(filename);
-
     for(Transition transition : transitions)
     {
         results << transition.sourceState;
-
         results << " ";
         for(char transitionChar : transition.transitionChars)
             results << transitionChar;
         results << " ";
-
         for(int targetState : transition.targetStates)
         {
             results << " ";
@@ -109,12 +98,11 @@ int main()
 {
     char a;
     buildnfa();
-
     unordered_set<char> alphas;
     vector<vector<int>> epsilonTransitions;
     vector<int> currentEpsilonTransitions;
     epsilonTransitions.resize(maxState);
-    vector<int> * epsilonTransitonsForCurrentState;
+    vector<int>* epsilonTransitonsForCurrentState;
 
     //Get the epsilon transitions for all of them
     for(int stateNumber = 0; stateNumber < maxState; ++stateNumber)
@@ -128,15 +116,13 @@ int main()
     //Calculate e*<target transition> e* for all states...
     vector<Transition> nfaWithoutEpsilonTransitions;
     Transition transition;
-    vector<int> * currentEpsilonTransitionState = nullptr;
+    vector<int>* currentEpsilonTransitionState = nullptr;
     vector<int> results;
-
     for(int stateNumber = 0; stateNumber < maxState; stateNumber++)
     {
         for(char currentTransitionChar : alphas)
         {
             currentEpsilonTransitionState = &epsilonTransitions[stateNumber];
-
             for(int epsilonState : *currentEpsilonTransitionState)
                 addTransitionsFromMatchingTargetChar(epsilonState, currentTransitionChar, results);
 
@@ -148,7 +134,6 @@ int main()
             }
         }
     }
-
     exportToFile("results.txt", nfaWithoutEpsilonTransitions);
 }
 
