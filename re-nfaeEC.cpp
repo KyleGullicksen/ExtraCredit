faae29827e4b9154ad21950ef01c8c377372970a
Kyle Gullicksen <gulli003@cougars.csusm.edu>
2017-11-21 14:57:47 -0800

    Changes

--- re-nfaeEC.cpp
+++ re-nfaeEC.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <climits>
 
 using namespace std;
+
+vector<Machine> machines;
+int stateNumberCounter = 0;
 
 #define NO_TRANSITION '*'
 #define NO_STATE -1
@@ -10,25 +16,136 @@ using namespace std;
 
 struct Transition
 {
-    int source = 0;
-    char acceptedCharacter = 'a';
-    vector<int> destinations;
+    int source;
+    unordered_set<char> acceptedChars;
+    int destination;
 };
 
-vector<vector<Transition>> machines;
-int stateIndex = 0;
-
-// ------------- Put utility functions here --------------------
-
-Transition makeTransition(int sourceState, char acceptedChar, int target)
+struct Node
 {
-    Transition transition;
-    transition.source = sourceState;
-    transition.acceptedCharacter = acceptedChar;
-    transition.destinations.push_back(target);
+    int number;
+    unordered_map<int, Transition> transitions;
+};
 
-    return transition;
+class Machine
+{
+protected:
+    int start = INT_MAX;
+    int end = INT_MIN;
+    unordered_map<int, Node> nodes;
+public:
+    void add(Node node)
+    {
+        //new start?
+        if(node.number < start)
+            start = node.number;
+
+        //new end?
+        if(node.number > end)
+            end = node.number;
+
+        nodes[node.number] = node;
+    }
+
+    Node & get(int number)
+    {
+        return nodes.at(number);
+    }
+
+    Node & getStartNode()
+    {
+        return nodes.at(start);
+    }
+
+    Node & getEndNode()
+    {
+        return nodes.at(end);
+    }
+
+    void appendTransition(int nodeNumber, Transition transition)
+    {
+        if(nodes.find(nodeNumber) == nodes.end())
+            return;
+
+        Node& selectedNode = nodes.at(nodeNumber);
+
+        if(selectedNode.transitions.find(transition.destination) == selectedNode.transitions.end())
+        {
+            selectedNode.transitions[transition.destination] = transition;
+            return;
+        }
+
+        Transition & currentTransition = selectedNode.transitions[transition.destination];
+        for(char c : transition.acceptedChars)
+            currentTransition.acceptedChars.insert(c);
+    }
+
+    template <typename T>
+    void visit(T&& callback)
+    {
+        Node currentNode;
+
+        for(auto element : nodes)
+        {
+            currentNode = element.second;
+            callback(currentNode);
+        }
+    }
+
+    virtual void link(int source, char transitionChar, int destination)
+    {
+        Transition transition;
+        transition.source = source;
+        transition.acceptedChars.push_back(transitionChar);
+        transition.destination = destination;
+        link(transition);
+    }
+
+    virtual void link(Transition transition)
+    {
+        if(nodes.find(transition.source) == nodes.end())
+        {
+            Node start;
+            start.number = transition.source;
+            add(start);
+        }transition.acceptedCharacters.push_back(transitionChar);
+        if(nodes.find(transition.destination) == nodes.end())
+        {
+            Node end;
+            end.number = transition.destination;
+            add(end);
+        }
+
+        Node & node = nodes.at(transition.source);
+    }
+
+    Node nextNode()
+    {
+        Node nextNode;
+        nextNode.number = stateNumberCounter++;
+
+        return nextNode;
+    }
+
+    int getStartNumber()
+    {
+        return start;
+    }
+
+    int getEndNumber()
+    {
+        return end;
+    }
+};
+
+int nextStateNumber()
+{
+    int next = stateNumberCounter;
+    ++stateNumberCounter;
+
+    return next;
 }
+
 
 void processConcat()
 {
@@ -59,12 +176,6 @@ void processOr()
         cout << "Enter different number for the second machine";
         cin >> M2;
     }
-
-    vector<Transition> & firstMachine = machines[M1];
-    vector<Transition> & secondMachine = machines[M2];
-
-    //Need a new start, and a new end
-    Transition start = makeTransition();
 }
 
 void processStar()
@@ -73,17 +184,6 @@ void processStar()
     cout << "Enter number of the machine:";
     cin >> M1;
 
-    //get the requested machine
-    vector<Transition> & machine = machines[M1];
-
-    //Make a new start state [linking this new start state to the old start state]
-    Transition & oldStart = machine[0];
-    Transition & oldEnd = machine[machine.size() - 1];
-
-    //Make a new starting node
-    Transition start = makeTransition(stateIndex++, EPSILON, oldStart.source);
-    start.destinations.push_back(stateIndex);
-    oldEnd.destinations.push_back(stateIndex);
 }
 
 void processPlus()
@@ -92,16 +192,43 @@ void processPlus()
     cout << "Enter number of the machine:";
     cin >> M1;
 
+    Machine & machine = machines.at(M1);
 
+    int oldStart = machine.getStartNumber();
+    int oldEnd = machine.getEndNumber();
 
-    // create new start
-    // create new final
-    // For the new machine in NFA:
-    // add transition from new start to M1 start
-    // add M1
-    // add transition (back) from M1 end to M1 start
-    // add transition from M1 end to new final
+    int newStart = nextStateNumber();
+    int newEnd = nextStateNumber();
 
+    machine.link(newStart, EPSILON, oldStart);
+    machine.link(oldEnd, EPSILON, newEnd);
+    machine.link(newEnd, EPSILON, newStart);
+}
+
+void exportToFile()
+{
+    Node currentNode;
+    ofstream output("reToNFAEOutput.txt", ios::out);
+
+    for(Machine machine : machines)
+    {
+        //Export each machine to file...
+
+        //Format: <source> <transition chars> <destination> newline
+        machine.visit([](Node node) ->void {
+            for(Transition transition : node.transitions)
+            {
+                output << node.number;
+                output << " ";
+                output << transition.acceptedCharacters;
+                output << " ";
+                output << transition.destination;
+                output << "\n";
+            }
+        });
+    }
+
+    output.close();
 }
 
 int main()
@@ -110,37 +237,31 @@ int main()
     cout << "But it is done interactively by asking you to apply operators" << endl;
     cout << "to component machines." << endl;
 
-    // build a transition for arrow label c
-    // display the transition
-    // add it to NFA
+
+    ifstream input("re.txt", ios::in);
     char c;
-    vector<char> alphabet;
-    Transition start;
-    Transition end;
-    vector<Transition> currentMachine;
+    int start = 0;
+    int end = 0;
 
-    ifstream fin("re.txt", ios::in);
-    while(fin >> c)
+    //parse the file to get our initial machines
+    while(input >> c)
     {
-        currentMachine.clear();
+        Machine machine;
 
-        // build a transition for arrow label c
-        start.source = stateIndex++;
-        start.acceptedCharacter = c;
-        start.destinations.push_back()
+        start = nextStateNumber();
+        end = nextStateNumber();
 
-        currentMachine.push_back(start);
+        machine.link(start, c, end);
 
-        machines.push_back(currentMachine);
-
-        // display the transition
-//            0---a---1
-//            Initial=0
-//            Final=1
-
-        cout << start.source << "--" << start.acceptedCharacter << "--" << start.destinstion << endl;
+        //Display the transiton
+        cout << start << "--" << c << "--" << end << endl;
+        cout << "Initial=" << start << endl;
+        cout << "Final=" << end << endl;
+        cout << "" << endl;
     }
-    fin.close();
+
+    input.close();
+
     char A;
     while(A != 'n')
     {
@@ -159,4 +280,6 @@ int main()
     cout << "Outputting all machines to nfa-e.txt... " << endl;
     ofstream fout("nfa-e.txt", ios::out);
     // send all NFA contents to the output file
+
+    exportToFile();
 }
